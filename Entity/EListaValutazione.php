2331594176ce1900350690f5f17ab3f59ab32ca4
Andrea <andrea.cocci@live.it>
2017-05-23 12:22:06 +0200

    Aggiunto EListaValutazione
    Inseriti attributi, get, set e metodo per inserire in coda all'array di
    valutazioni.

---
+++ Entity/EListaValutazione.php
@@ -0,0 +1,21 @@
+<?php
+    class EListaValutazione {
+
+        // ATTRIBUTI //
+        private $media;
+        private $listaVal = array();
+
+        // METODI //
+        public function getMedia() {
+            return $this->media;
+        }
+
+        public function setMedia($media) {
+            $this->media = $media;
+        }
+
+        public function addArrayElement(EValutazione $val) {
+            array_push($this->listaVal, $val);
+        }
+    }
+?>
