43a7eb1b986f3a5d57b889743de2a63f0e6203ab
simoleo95 <leonardisimonemaria@gmail.com>
2017-06-05 20:33:34 +0200

    Aggiunta di EPrezzo e modifica di EAsta

--- Lastminute/Entity/EAsta.php
+++ Lastminute/Entity/EAsta.php
@@ -1,86 +1,42 @@
 <?php
 
 
-class EPersona{
+class EAsta{
     //ATTRIBUTI
-    private $nome;
-    private $cognome;
-    private $email;
-    private $datanascita;
-   // private $luogodinacita;
-    private $username;
-    private $password;
+    private $IdAsta;
+    private $DataInizio;
+    private $DataFine;
 
 
     // costruttore a cui vengono passsati dei parametri
-    function __construct($nome, $cognome, $email, $datanascita, $luogodinacita, $username, $password) {
-        $this->nome = $nome;
-        $this->cognome = $cognome;
-        $this->email = $email;
-        $this->datanascita = $datanascita;
-        $this->luogodinacita = $luogodinacita;
-        $this->username = $username;
-        $this->password = $password;
-
-
-
+    function __construct($Id, $Di, $Df) {
+        $this->IdAsta = $Id;
+        $this->DataInizio = $Di;
+        $this->DataFine = $Df;
     }
     //METODI
-    function getNome() {
-        return $this->nome;
+    function getIdAsta() {
+        return $this->IdAsta;
     }
 
-    function getCognome() {
-        return $this->cognome;
+    function getDataInizio() {
+        return $this->DataInizio;
     }
 
-    function getEmail() {
-        return $this->email;
+    function getDataFine() {
+        return $this->DataFine;
     }
 
-    function getDatanascita() {
-        return $this->datanascita;
+    function setIdAsta($id) {
+        $this->IdAsta = $id;
     }
 
-    function getLuogodinacita() {
-        return $this->luogodinacita;
+    function setDataInizio($Di) {
+        $this->DataInizio = $Di;
     }
 
-    function getUsername() {
-        return $this->username;
+    function setDataFine($Df) {
+        $this->DataFine = $Df;
     }
 
-    function getPassword() {
-        return $this->password;
-    }
-
-    function setNome($nome) {
-        $this->nome = $nome;
-    }
-
-    function setCognome($cognome) {
-        $this->cognome = $cognome;
-    }
-
-    function setEmail($email) {
-        $this->email = $email;
-    }
-
-    function setDatanascita($datanascita) {
-        $this->datanascita = $datanascita;
-    }
-
-  /* function setLuogodinacita($luogodinacita) {
-  /    $this->luogodinacita = $luogodinacita;
-    }*/
-
-    function setUsername($username) {
-        $this->username = $username;
-    }
-
-    function setPassword($password) {
-        $this->password = $password;
-
-        }
-    }
 ?>
---
+++ Lastminute/Entity/EPrezzo.php
@@ -0,0 +1,32 @@
+<?php
+
+
+class EAsta{
+    //ATTRIBUTI
+    private $Importo;
+    private $Valuta;
+
+    // costruttore a cui vengono passsati dei parametri
+    function __construct($Im, $Va) {
+        $this->Importo = $Im;
+        $this->Valuta = $Va;
+    }
+    //METODI
+    function getImporto() {
+        return $this->Importo;
+    }
+
+    function getValuta() {
+        return $this->Valuta;
+    }
+    }
+
+    function setImporto($Im) {
+        $this->Importo = $Im;
+    }
+
+    function setValuta($Va) {
+        $this->Valuta = $Va;
+    }
+
+?>
