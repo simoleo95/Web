c56443248c6d8965ea77ce8329f283784029e7da
Andry695 <andrea.cocci@live.it>
2017-07-25 16:31:51 +0200

    Aggiunto 'includes'
    Aggiunta la cartella 'includes' che contengono la pagina con le
    impostazioni di configurazione e la pagina con la funzione di autoload
    delle classi

---
+++ Lastminute/includes/autoload.inc.php
@@ -0,0 +1,20 @@
+<?php
+    spl_autoload_register(function ($class_name) {
+        switch ($class_name[0]) {
+            case 'V':
+                require_once ('View/'.$class_name.'.php');
+                break;
+            case 'F':
+                require_once ('Foundation/'.$class_name.'.php');
+                break;
+            case 'E':
+                require_once ('Entity/'.$class_name.'.php');
+                break;
+            case 'C':
+                require_once ('Controller/'.$class_name.'.php');
+                break;
+            case 'U':
+                require_once ('Foundation/Utility/'.$class_name.'.php');
+        }
+    });
+?>
---
+++ Lastminute/includes/config.inc.php
@@ -0,0 +1,8 @@
+<?php
+    global $config;
+
+    $config['mysql']['dbms']='localhost';
+    $config['mysql']['dbname']='web';
+    $config['mysql']['host']='root';
+    $config['mysql']['password']='';
+?>
