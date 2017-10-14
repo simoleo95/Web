9c84d573ef8d389abf7fae7d7b9b275abb1e21ef
rino95 <otto9508@libero.it>
2017-10-14 11:54:57 +0200

    scroll funzionante!!!

--- Lastminute/templates/templates/js/scroll.js
+++ Lastminute/templates/templates/js/scroll.js
@@ -1,14 +1,24 @@
 $(document).ready(function () {
-  var called = 1;
-	
-    $(window).scroll(function() {		    
-    if($(window).scrollTop() == $(document).height() - $(window).height()) {			
+  
 
-            var url = "index.php?ajax=true&num="+(called++ * 6);		
+     var called = 1;	
+    $(window).scroll(function() {
+		var difference= $(document).height() - $(window).height();
+		var difference= difference-1;
+		var top =$(document).scrollTop();
+		
+		
+       if(top >= difference ) {
+	   
+	   
+	   
+            var url = "index.php?ajax=true&num="+(called++ * 6);
+		   
 
-             $.ajax({
+           $.ajax({
                 url: url,
                 success: function(result) {
+					
                     $('#ricerca').append(result);
                 },
                 error: function() {
